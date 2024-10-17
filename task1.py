4014b624e79cad2e1dcd5c51997b8d3f83ff5027
Logan Costello <logancostello2022@gmail.com>
2024-10-16 17:27:17 -0700

    mostly through task1

--- task1.py
+++ task1.py
@@ -1,15 +1,17 @@
 import hashlib
+import time
+import random
+import string
 
-# Task 1a:
-def sha256_hash(string, numBits):
+# Hashes the input string, returns only numBits bits of the hashed value
+def sha256_hash(inputStr, numBits):
     # str to bytes
-    encoded_string = string.encode('utf-8')
+    encoded_string = inputStr.encode('utf-8')
 
     sha256 = hashlib.sha256(encoded_string)
 
     # string -> int
     hashed = sha256.hexdigest()
-    print(hashed)
     hashed = int(hashed, 16)
 
     # get only the last numBits bits
@@ -19,12 +21,37 @@ def sha256_hash(string, numBits):
     # int -> hex
     hashed = hex(hashed)
 
-    print(hashed)
+    return hashed
 
+# Finds two different strings that collide when hashed, given numBits bits of the hashed values
+def findCollision(numBits):
+    # map hashes to their strings
+    hashes = {}
+    numStringsTried = 0
+    startTime = time.time()
+    while True:
+        # generate random string
+        strLen = 10
+        randomString = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(strLen))
+        hashed_value = sha256_hash(randomString, numBits)
+        numStringsTried += 1
+        if hashed_value in hashes:
+            if randomString != hashes[hashed_value]:
+                endTime = time.time()
+                totalTime = endTime - startTime
+                totalTime = round(totalTime, 6)
+                print(f"Solution for {numBits} bits:")
+                print(f"Word 1: {randomString}")
+                print(f"Word 2: {hashes[hashed_value]}")
+                print(f"Collision: {hashed_value}")
+                print(f"{numStringsTried} strings tried in {totalTime} seconds")
+                print()
+                return [randomString, hashes[hashed_value], numStringsTried, totalTime]
+        else:
+            hashes[hashed_value] = randomString
 
 if __name__ == '__main__':
-    a = "aaa"
-    b = "aab"
-    numBits = 8
-    sha256_hash(a, numBits)
-    sha256_hash(b, numBits)
+    findCollision(8)
+    findCollision(10)
+    findCollision(12)
+
