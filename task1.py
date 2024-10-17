6d00388ceff00b2080c65d5b4f2afc13e804c854
Logan Costello <logancostello2022@gmail.com>
2024-10-16 17:02:50 -0700

    a bit through task 1c

--- task1.py
+++ task1.py
@@ -1,3 +1,30 @@
+import hashlib
+
+# Task 1a:
+def sha256_hash(string, numBits):
+    # str to bytes
+    encoded_string = string.encode('utf-8')
+
+    sha256 = hashlib.sha256(encoded_string)
+
+    # string -> int
+    hashed = sha256.hexdigest()
+    print(hashed)
+    hashed = int(hashed, 16)
+
+    # get only the last numBits bits
+    mask = pow(2, numBits) - 1
+    hashed &= mask
+
+    # int -> hex
+    hashed = hex(hashed)
+
+    print(hashed)
+
 
 if __name__ == '__main__':
-    print("Hello world")
+    a = "aaa"
+    b = "aab"
+    numBits = 8
+    sha256_hash(a, numBits)
+    sha256_hash(b, numBits)
